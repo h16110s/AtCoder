1d54d7b4d1113f1570b11a49f32f6559d4009ca2
Hirohito <h_16110s@yahoo.co.jp>
2019-02-20 16:14:51 +0900

    update Character code to UTF-8 AtCoder_Beginner_Contest032_A-B-C

---
+++ AtCoder_Beginner_Contest_032/A.cpp
@@ -0,0 +1,24 @@
+﻿/**A - 高橋君と青木君の好きな数	C++14 (GCC 5.4.1)
+	Accepted
+**/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	long int n;
+	cin >> a;
+	cin >> b;
+	cin >> n;
+
+	int count = n;
+	while (true) {
+		if (count % a == 0 && count % b == 0) {
+			break;
+		}
+		count++;
+	}
+	cout << count << endl;
+	return 0;
+}
---
+++ AtCoder_Beginner_Contest_032/C.cpp
@@ -0,0 +1,63 @@
+﻿/**
+	C - 列 	C++14 (GCC 5.4.1)
+	Accepted
+**/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+long int max(long int a, long int b) {
+	if (a > b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+
+int main() {
+	unsigned long int N;
+	unsigned long int K;
+	bool grater = true;
+	bool zero = false;
+
+	cin >> N >> K;
+	vector<unsigned long int> s(N);
+
+	for (unsigned long int i = 0; i < N; i++) {
+		cin >> s[i];
+		if (s[i] <= K) {
+			grater = false;
+		}
+		if (s[i] == 0) {
+			zero = true;
+		}
+	}
+	if (grater) {
+		cout << 0 << endl;
+		exit(0);
+	}
+	if(zero) {
+		cout << N << endl;
+		exit(0);
+	}
+
+	long int ans = 0;
+	long int right , left;
+	for (left = 0; left < N - ans; left++) {
+		long int count = 0;
+		long long x = 1;
+		for (right = left; right < N; right++) {
+			if (x * s[right] > K) {
+				break;
+			}
+			x *= s[right];
+			count++;
+		}
+		//cout << "x:" << x << endl;
+		ans = max(ans, count);
+	}
+	cout << ans << endl;
+}
