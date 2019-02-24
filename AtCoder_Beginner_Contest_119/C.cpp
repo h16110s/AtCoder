0561842edd4d5daddf97a1087e1b162e84145fe5
hiro-160 <h_16110s@yahoo.co.jp>
2019-02-25 00:49:29 +0900

    AtCoder_Beginner_Contest119_A-B

---
+++ AtCoder_Beginner_Contest_119/C.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int N,A,B,C;
+
+int main(){
+    int step = 0;
+    bool a = true,b = true ,c = true;
+    vector<int> bam;
+    cin >> N >> A >> B >> C;
+    
+    for (int i = 0 ; i < N ; i++){
+        int tmp = 0;
+        cin >> tmp;
+        bam.push_back(tmp);
+    }
+
+
+
+}
